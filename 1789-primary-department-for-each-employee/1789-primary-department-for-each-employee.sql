88d7c80e9e2fc4ee3fa41ab146a4236d9b43781b
svarshithreddy28 <125176540+svarshithreddy28@users.noreply.github.com>
2026-09-22 21:41:16 +0530

    Time: 934 ms (6.44%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -3,7 +3,7 @@ select e1.employee_id,e1.department_id
 from employee e1
 group by e1.employee_id
 having count(*)=1
-union all
+union 
 select e2.employee_id,e2.department_id
 from employee e2
 where primary_flag='Y';
